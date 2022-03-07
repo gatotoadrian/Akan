34e907d495d7967036268ab6f44d8843660be085
gatotoadrian <gatotoadrian@gmail.com>
2022-03-07 17:59:12 +0300

    modified file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-// collect data from HTML
-const formData = document.getElementById("submit");
-const displayName = document.querySelector(".output"); // displaying akan names on the desired position
-const userGender = document.querySelectorAll("#gender");
-const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-const femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-formData.addEventListener("click", function (ceaseAction) {
-ceaseAction.preventDefault();
-const dateInput = document.getElementById("date").value;
-const date = new Date(dateInput);
-const year = date.getFullYear();
-const day = date.getDay();
-// console.log(maleAkanNames[day]);
-// displayName.innerHTML = `${maleAkanNames[day]}`;
-// console.log(typeof userGender);
-for(let choice of userGender){
-// console.log(choice.value);
+const submition = document.getElementById("button");
+
+const submitionDate = document.querySelector(".output");
+
+const sex = document.querySelectorAll("#action");
+
+const males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+const females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+const days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Satruday"];
+
+
+submition.addEventListener("click", function (terminateAction) {
+terminateAction.preventDefault();
+const newDate = document.getElementById("date").value;
+const setDate = new Date(newDate);
+const initialYear = setDate.getFullYear();
+const newEntry = setDate.getDay();
+for(let choice of gender){
 if (choice.value === "male"){
-// console.log(`${maleAkanNames[day]}`);
-displayName.innerHTML = `${maleAkanNames[day]}`;
+submitionDate.innerHTML = `Here is your Akan name: ${males[newEntry]}`;
 }
 if (choice.value === "female"){
-// console.log(`${femaleAkanNames[day]}`);
-displayName.innerHTML = `${femaleAkanNames[day]}`;
+submitionDate.innerHTML = `Here is your Akan name: ${females[newEntry]}`;
 }
 }
 })
